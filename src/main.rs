f7061417661aac1ac00db74a96e4abca00937983
Minh <propfeds@gmail.com>
2024-04-26 16:34:31 +0900

    Implemented keyboard movement

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,105 @@
-use rltk::{Rltk, GameState};
+use rltk::{GameState, RGB, Rltk, VirtualKeyCode};
+use specs::prelude::*;
+use std::cmp::{max, min};
+use specs_derive::Component;
 
-struct State {}
+#[derive(Component)]
+struct Position
+{
+    x: i32,
+    y: i32
+}
+
+#[derive(Component)]
+struct Renderable
+{
+    glyph: rltk::FontCharType,
+    fg: RGB,
+    bg: RGB
+}
+
+#[derive(Component)]
+struct LeftMover {}
+
+struct State
+{
+    ecs: World
+}
+
 impl GameState for State
 {
     fn tick(&mut self, ctx: &mut Rltk)
     {
+        player_input(self, ctx);
+        self.run_systems();
+
         ctx.cls();
-        ctx.print(1, 1, "Hello Fellow Crabbos");
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+
+        for (pos, render) in (&positions, &renderables).join()
+        {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
+    }
+}
+
+struct LeftWalker {}
+
+impl<'a> System<'a> for LeftWalker
+{
+    type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
+    fn run(&mut self, (lefty, mut pos): Self::SystemData)
+    {
+        for (_lefty, pos) in (&lefty, &mut pos).join()
+        {
+            pos.x -= 1;
+            if pos.x < 0
+            {
+                pos.x = 79;
+            }
+        }
+    }
+}
+
+#[derive(Component, Debug)]
+struct Player {}
+
+fn try_move_player(dx: i32, dy: i32, ecs: &mut World)
+{
+    let mut positions = ecs.write_storage::<Position>();
+    let mut players = ecs.write_storage::<Player>();
+
+    for (_player, pos) in (&mut players, &mut positions).join()
+    {
+        pos.x = min(79, max(0, pos.x + dx));
+        pos.y = min(49, max(0, pos.y + dy));
+    }
+}
+
+fn player_input(gs: &mut State, ctx: &mut Rltk)
+{
+    match ctx.key
+    {
+        None => {}
+        Some(key) => match key
+        {
+            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
+            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+            _ => {}
+        }
+    }
+}
+
+impl State
+{
+    fn run_systems(&mut self)
+    {
+        let mut lw = LeftWalker{};
+        lw.run_now(&self.ecs);
+        self.ecs.maintain();
     }
 }
 
@@ -14,8 +107,36 @@ fn main() -> rltk::BError
 {
     use rltk::RltkBuilder;
     let context = RltkBuilder::simple80x50()
-    .withTitle("Roguelike Tut")
+    .with_title("Roguelike Tut")
     .build()?;
-    let gs = State{ };
+    let mut gs = State{ecs: World::new()};
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
+    gs.ecs.register::<LeftMover>();
+    gs.ecs.register::<Player>();
+
+    gs.ecs.create_entity()
+    .with(Position{x: 40, y: 25})
+    .with(Renderable
+    {
+        glyph: rltk::to_cp437('@'),
+        fg: RGB::named(rltk::YELLOW),
+        bg: RGB::named(rltk::BLACK)
+    })
+    .with(Player{})
+    .build();
+    for i in 0..=10
+    {
+        gs.ecs.create_entity()
+        .with(Position{x: i * 7, y: 20})
+        .with(Renderable
+        {
+            glyph: rltk::to_cp437('☺'),
+            fg: RGB::named(rltk::RED),
+            bg: RGB::named(rltk::BLACK)
+        })
+        .with(LeftMover{})
+        .build();
+    }
     rltk::main_loop(context, gs)
 }
